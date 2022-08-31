50e8135412b73caeeeeda190b353f9d8c4f137e5
gabriel-josantos <gabriel.josantos1@gmail.com>
2022-08-31 18:57:27 -0300

    Implemented CORS

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
-
+const cors = require('cors');
 // App Modules
 
 const AppError = require('./utils/appError');
@@ -46,6 +46,18 @@ app.set('views', path.join(__dirname, 'views'));
 
 //////////////////////////////////////////////
 // 1) GLOBAL MIDDLEWARES
+// Implement cors
+app.use(cors());
+//Access-Control-Allow-Origin
+// api.natours.com,front-end:natours.com
+// app.use(
+//   cors({
+//     origin: 'https://ww.natours.com',
+//   })
+// );
+
+app.options('*', cors());
+//app.options('api/v1/tours/:id',cors());
 
 //Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
