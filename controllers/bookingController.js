fe227b93c8dd0d637fb656903a8d359f5d733454
gabriel-josantos <gabriel.josantos1@gmail.com>
2022-09-01 02:37:20 -0300

    m

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -18,7 +18,7 @@ exports.getCheckoutSession = catchAsync(async function (req, res, next) {
     success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
-    client_reference_id: req.params.tourID,
+    client_reference_id: req.params.tourId,
 
     line_items: [
       {
