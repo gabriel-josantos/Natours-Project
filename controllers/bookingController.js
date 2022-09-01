4292e63757b356349aa8a278133e35318173756f
gabriel-josantos <gabriel.josantos1@gmail.com>
2022-09-01 00:52:02 -0300

    m

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -61,7 +61,7 @@ exports.getCheckoutSession = catchAsync(async function (req, res, next) {
 
 async function createBookingCheckout(session) {
   const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
+  const user = (await User.findOne({ email: session.customer_email }))._id;
   const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 }
